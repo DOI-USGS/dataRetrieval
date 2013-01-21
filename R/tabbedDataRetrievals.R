5d80bc4e32cf588602ca64577fe713c9eb85aa57
Laura DeCicco <ldecicco@usgs.gov>
2013-01-21 10:13:13 -0600

    Took out flux unit from data retrieval.  I think it is only relevent in EGRET.

--- R/tabbedDataRetrievals.R
+++ R/tabbedDataRetrievals.R
@@ -45,28 +45,6 @@ NULL
 #' @keywords water flow data
 NULL
 
-#' Flux units included in dataRetrieval
-#'
-#' Flux units included:
-#' \tabular{lllll}{
-#' Number \tab ObjectName \tab shortName \tab unitFactor \tab unitName  \cr
-#' 1      \tab POUNDS_DAY \tab lbs/day   \tab 2.204623   \tab pounds/day\cr
-#' 2      \tab TONS_DAY   \tab tons/day  \tab 0.001102  \tab tons/day   \cr
-#' 3      \tab KG_DAY     \tab kg/day    \tab 1          \tab kg/day    \cr
-#' 4      \tab THOUSAND_KG_DAY\tab 10^3 kg/day \tab 0.001 \tab "thousands of kg/day\cr
-#' 5      \tab TONS_YEAR\tab tons/yr \tab 0.402619 \tab tons/year\cr
-#' 6      \tab THOUSAND_TONS_YEAR\tab 10^3 tons/yr \tab 0.000402619 \tab thousands of tons/year \cr
-#' 7      \tab MILLION_TONS_YEAR\tab 10^6 tons/yr \tab 4.02619e-07 \tab millions of tons/year\cr
-#' 8      \tab THOUSAND_KG_YEAR\tab 10^3 kg/yr \tab 0.36525 \tab thousands of kg/year\cr
-#' 9      \tab MILLION_KG_YEAR\tab 10^6 kg/yr \tab 0.00036525 \tab millions of kg/year\cr
-#' 10     \tab BILLION_KG_YEAR\tab 10^9 kg/yr \tab 3.6525e-07 \tab billions of kg/year \cr
-#' }
-#' 
-#'
-#' @name FLUX_UNIT
-#' @docType data
-NULL
-
 #' Example Sample Dataframe included in dataRetrieval
 #'
 #' Initial Sample data frame from the Choptank River
