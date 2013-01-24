15e941127d28b59392daef7e962e9ef1e34660b3
Laura DeCicco <ldecicco@usgs.gov>
2013-01-24 11:45:05 -0600

    Pulling in data as string, skipping checks for numbers and dates.

--- R/getSiteFileData.r
+++ R/getSiteFileData.r
@@ -11,11 +11,15 @@
 #' # These examples require an internet connection to run
 #' siteINFO <- getSiteFileData('05114000',interactive=FALSE)
 getSiteFileData <- function(siteNumber="",interactive=TRUE){
+  
+  # Checking for 8 digit site ID:
   siteNumber <- formatCheckSiteNumber(siteNumber, interactive=interactive)
+  
   urlSitefile <- paste("http://waterservices.usgs.gov/nwis/site?format=rdb&siteOutput=Expanded&sites=",siteNumber,sep = "")
+  
   SiteFile <- read.delim(  
     urlSitefile, 
-    header = FALSE, 
+    header = TRUE, 
     quote="\"", 
     dec=".", 
     sep='\t',
@@ -23,22 +27,9 @@ getSiteFileData <- function(siteNumber="",interactive=TRUE){
     fill = TRUE, 
     comment.char="#")
   
-  col.nm <- make.names(unlist(SiteFile[1,, drop=TRUE]), allow_=FALSE)
-  retval <- lapply(SiteFile, function(x) {
-    Typ <- x[2] # The type
-    x <- x[-c(1,2)] # the data
-    if(regexpr('d$', Typ) > 0) { # Must be date
-      ret.val <- try(as.Date(x)) # The data are in standard format, but...
-      if(class(ret.val) == "try-error")
-        ret.val <- x
-    }
-    else if(regexpr('n$', Typ) > 0) # Must be numeric
-      ret.val <- as.numeric(x)
-    else # Must be character
-      ret.val <- x
-    return(ret.val)})
-  INFO <- as.data.frame(retval, stringsAsFactors=FALSE)
-  names(INFO) <- col.nm
+  INFO <- SiteFile[-1,]
+  names(INFO) <- gsub("_",".",names(INFO))
+  
   INFO$queryTime <- Sys.time()
   INFO$dec.lat.va <- as.numeric(INFO$dec.lat.va)
   INFO$dec.long.va <- as.numeric(INFO$dec.long.va)
