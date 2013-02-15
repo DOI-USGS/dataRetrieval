4ea56060b131017ad8c8f7f410a344f266e013b9
Laura DeCicco <ldecicco@usgs.gov>
2013-02-15 11:47:07 -0600

    Cleaned up qualifer logic.

--- R/processQWData.r
+++ R/processQWData.r
@@ -15,24 +15,9 @@
 #' rawSampleSelect <- processQWData(rawSample)
 processQWData <- function(data){
 
-    qualifier <- ifelse((rawData$ResultDetectionConditionText == "Not Detected" | 
-           rawData$ResultDetectionConditionText == "Detected Not Quantified" |
-           data$ResultMeasureValue < data$DetectionQuantitationLimitMeasure.MeasureValue),"<","")
-                        
-    
-#   qualifier <- ifelse(
-#     (
-#       (
-#         data$ResultDetectionConditionText == "Not Detected"
-#         & length(grep("Lower", data$DetectionQuantitationLimitTypeName)) > 0
-#       )
-#     | 
-#       (
-#         data$ResultMeasureValue < data$DetectionQuantitationLimitMeasure.MeasureValue
-#         & data$ResultValueTypeName == "Actual"
-#       )
-#     ),"<",""
-#   )
+  qualifier <- ifelse((data$ResultDetectionConditionText == "Not Detected" | 
+                    data$ResultDetectionConditionText == "Detected Not Quantified" |
+                    data$ResultMeasureValue < data$DetectionQuantitationLimitMeasure.MeasureValue),"<","")
   
   correctedData<-ifelse((nchar(qualifier)==0),data$ResultMeasureValue,data$DetectionQuantitationLimitMeasure.MeasureValue)
   test <- data.frame(data$USGSPCode)
