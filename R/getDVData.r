9cc2ebc2551237725d5b721e08e75196d8aadd5d
Laura DeCicco <ldecicco@usgs.gov>
2013-04-23 09:55:31 -0500

    Automatic roxygen updates.

--- R/getDVData.r
+++ R/getDVData.r
@@ -11,6 +11,9 @@
 #' @param interactive logical Option for interactive mode.  If true, there is user interaction for error handling and data checks.
 #' @param convert logical Option to include a conversion from cfs to cms (35.314667). The default is TRUE, 
 #' which is appropriate for using NWIS data in the EGRET package.  Set this to FALSE to not include the conversion.
+#' @param format string, can be "tsv" or "xml", and is only applicable for daily and unit value requests.  "tsv" returns results faster, but there is a possiblitiy that an incomplete file is returned without warning. XML is slower, 
+#' but will offer a warning if the file was incomplete (for example, if there was a momentary problem with the internet connection). It is possible to safely use the "tsv" option, 
+#' but the user must carefully check the results to see if the data returns matches what is expected. The default is "tsv".
 #' @keywords data import USGS WRTDS
 #' @export
 #' @return Daily dataframe
@@ -18,8 +21,8 @@
 #' @examples
 #' # These examples require an internet connection to run
 #' Daily <- getDVData('01594440','00060', '1985-01-01', '1985-03-31', interactive=FALSE)
-getDVData <- function (siteNumber,ParameterCd,StartDate,EndDate,interactive=TRUE,convert=TRUE){
-  data <- retrieveNWISData(siteNumber,ParameterCd,StartDate,EndDate,interactive=interactive)
+getDVData <- function (siteNumber,ParameterCd,StartDate,EndDate,interactive=TRUE,convert=TRUE,format="tsv"){
+  data <- retrieveNWISData(siteNumber,ParameterCd,StartDate,EndDate,interactive=interactive,format=format)
   
   #  need to setup conversion factor because the NWIS data are in cfs but we store in cms
   names(data) <- c('agency', 'site', 'dateTime', 'value', 'code')  # do a merge instead?
