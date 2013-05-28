c15bb429d7ce5fdeb0611c9b997f0b7a68c51d25
Laura DeCicco <ldecicco@usgs.gov>
2013-05-28 14:02:58 -0500

    Trying to fix leap year issue, forcing all March 1sts to be the 60th day of the year (and...all other dates likewise).

--- R/populateDateColumns.r
+++ R/populateDateColumns.r
@@ -6,7 +6,7 @@
 #' @return DateFrame dataframe
 #' @export
 #' @examples
-#' dateTime <- c('1985-01-01', '1985-01-02', '1985-01-03')
+#' dateTime <- c('1984-01-01', '1985-01-02', '1986-01-03')
 #' expandedDateDF <- populateDateColumns(dateTime)
 populateDateColumns <- function(rawData){  # rawData is a vector of dates
   DateFrame <- as.data.frame(matrix(ncol=1,nrow=length(rawData)))
@@ -17,6 +17,11 @@ populateDateColumns <- function(rawData){  # rawData is a vector of dates
   DateFrame$Month <- dateTime$mon + 1
   DateFrame$Day <- dateTime$yday + 1
   year <- dateTime$year + 1900
+  
+  leapOffset <- ifelse((year%%4 == 0) & ((year%%100 != 0) | (year%%400 == 0)), 0,1)
+  
+  DateFrame$Day[DateFrame$Day > 59] <- DateFrame$Day[DateFrame$Day > 59] + leapOffset 
+  
   DateFrame$DecYear <- year + (DateFrame$Day -0.5)/366
   DateFrame$MonthSeq <- ((year-1850)*12)+DateFrame$Month
   return (DateFrame)
