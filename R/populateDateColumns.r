ea1fc5f0d2b5dfc339e8ac7607b5e841635a23e9
Laura DeCicco <ldecicco@usgs.gov>
2013-06-17 15:45:27 -0500

    Fixed leapyear bug.

--- R/populateDateColumns.r
+++ R/populateDateColumns.r
@@ -20,7 +20,7 @@ populateDateColumns <- function(rawData){  # rawData is a vector of dates
   
   leapOffset <- ifelse((year%%4 == 0) & ((year%%100 != 0) | (year%%400 == 0)), 0,1)
   
-  DateFrame$Day[DateFrame$Day > 59] <- DateFrame$Day[DateFrame$Day > 59] + leapOffset 
+  DateFrame$Day[DateFrame$Day > 59] <- DateFrame$Day[DateFrame$Day > 59] + leapOffset[DateFrame$Day > 59]
   
   DateFrame$DecYear <- year + (DateFrame$Day -0.5)/366
   DateFrame$MonthSeq <- ((year-1850)*12)+DateFrame$Month
