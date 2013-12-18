8a4f8094083ca697ce42a3da4742a6806756574d
Laura DeCicco <ldecicco@usgs.gov>
2013-12-18 15:12:57 -0600

    Changed site check to only sites with less than 8 digits.

--- R/formatCheckSiteNumber.r
+++ R/formatCheckSiteNumber.r
@@ -1,6 +1,6 @@
 #' formatCheckSiteNumber
 #'
-#' Checks that the site code is 8 digits. If it is not 8 digits, it confirms with the user.
+#' Checks that the site code is at least 8 digits. If not, it confirms with the user.
 #' @param siteNumber string to check
 #' @param interactive logical Option for interactive mode.  If true, there is user interaction for error handling and data checks.
 #' @keywords WRTDS flow
@@ -9,15 +9,17 @@
 #' @examples
 #' site<- '01234567'
 #' formatCheckSiteNumber(site)
+#' site_incorrect <- '1234567'
+#' formatCheckSiteNumber(site_incorrect)
 formatCheckSiteNumber <- function(siteNumber, interactive=TRUE){  #checks for a 8 digit number
-  if (nchar(siteNumber) != 8){
+  if (nchar(siteNumber) < 8){
     if (interactive){
-      cat("Most common USGS site numbers are 8 digits long, you entered a ", nchar(siteNumber), "digit number = ", siteNumber , ".\n")
+      cat("Most common USGS site numbers are at least 8 digits long, you entered a ", nchar(siteNumber), "digit number = ", siteNumber , ".\n")
       cat("If you would like to change the site id, enter it here (no quotes), otherwise hit return:\n")
       tempSiteID <- readline()
       if (nzchar(tempSiteID)) siteNumber <- tempSiteID
     } else {
-      warningMessage <- paste("Most common USGS site numbers are 8 digits long, you entered ", siteNumber , ".", sep="")
+      warningMessage <- paste("Most common USGS site numbers are at least 8 digits long, you entered ", siteNumber , ".", sep="")
       warning(warningMessage)
     }
   }
