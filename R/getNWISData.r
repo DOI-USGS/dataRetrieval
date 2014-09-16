185c19c23059335075e6ef7b8d2bd4fe006d09eb
unknown <ldecicco@usgs.gov>
2014-09-16 11:40:09 -0500

    Updated doc.

--- R/getNWISData.r
+++ R/getNWISData.r
@@ -1,9 +1,10 @@
 #' General Data Import from NWIS
 #'
 #' Returns data from the NWIS web service.
-#' Arguments to the function should be based on \url{http://waterservices.usgs.gov/rest/Site-Service.html#Service}
+#' Arguments to the function should be based on \url{http://waterservices.usgs.gov} service calls.
 #'
-#' @param service string
+#' @param service string. Possible values are "iv" (for instantaneous), "dv" (for daily values), "gwlevels" 
+#' (for groundwater levels), and "qwdata" (for water quality)
 #' @param \dots see \url{http://waterservices.usgs.gov/rest/Site-Service.html#Service} for a complete list of options
 #' @keywords data import NWIS web service
 #' @return retval dataframe 
@@ -21,7 +22,7 @@ getNWISData <- function(service="dv", ...){
   
   baseURL <- paste0("http://waterservices.usgs.gov/nwis/",service,"/?format=rdb&")
   urlCall <- paste0(baseURL,urlCall)
-  if(service=="qw"){
+  if(service=="qwdata"){
     urlCall <- paste0(urlCall,"&siteOutput=expanded")
   }
   
