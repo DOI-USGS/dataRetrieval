b540dd2a8c6f39e9122cb0c75494216661370667
unknown <ldecicco@usgs.gov>
2014-12-02 12:29:47 -0600

    Added information of output.

--- R/tabbedDataRetrievals.R
+++ R/tabbedDataRetrievals.R
@@ -32,8 +32,23 @@ NULL
 #' format=rdb&show=parameter_group_nm&show=parameter_nm&show=casrn&show=srsname&show=parameter_units}
 #'
 #' @name parameterCdFile
+#' @return parameterData data frame with information about USGS parameters.
+#' 
+#' \tabular{lll}{
+#'   Name \tab Type \tab Description\cr
+#'   parameter_cd \tab character \tab 5-digit USGS parameter code \cr
+#'   parameter_group_nm \tab character \tab USGS parameter group name\cr
+#'   parameter_nm \tab character \tab USGS parameter name\cr
+#'   casrn \tab character \tab Chemical Abstracts Service (CAS) Registry Number\cr
+#'   srsname \tab character \tab Substance Registry Services Name\cr
+#'   parameter_units \tab character \tab Parameter units\cr
+#' }
+#' 
 #' @docType data
 #' @keywords USGS parameterCd
+#' @examples
+#' parameterCdFile <- parameterCdFile
+#' dischargeInfo <- parameterCdFile[parameterCdFile$parameter_cd=="00060",]
 NULL
 
 #' Data to convert USGS parameter code to characteristic names
@@ -42,6 +57,26 @@ NULL
 #' \url{http://www.waterqualitydata.us/public_srsnames?mimeType=json}.
 #'
 #' @name pCodeToName
+#' @return pCodeToName data frame with information about USGS parameters and how they
+#' relate to characteristic names (useful for WQP requests).
+#' 
+#' \tabular{lll}{
+#'   Name \tab Type \tab Description\cr
+#'   parm_cd \tab character \tab 5-digit USGS parameter code \cr
+#'   description \tab character \tab Parameter description\cr
+#'   characteristicname \tab character \tab Characteristic Name \cr
+#'   measureunitcode \tab character \tab Parameter units\cr
+#'   resultsamplefraction \tab character \tab Result sample fraction text\cr
+#'   resulttemperaturebasis \tab character \tab Temperature basis information\cr
+#'   resultstatisticalbasis \tab character \tab Statistical basis\cr
+#'   resulttimebasis \tab character \tab Time basis\cr
+#'   resultweightbasis \tab character \tab Weight basis\cr
+#'   resultparticlesizebasis \tab character \tab Particle size basis\cr
+#'   last_rev_dt \tab character \tab Latest revision of information\cr
+#' }
 #' @docType data
 #' @keywords USGS parameterCd
+#' @examples
+#' pCodeToName <- pCodeToName
+#' dischargeInfo <- pCodeToName[pCodeToName$parm_cd=="00060",]
 NULL
