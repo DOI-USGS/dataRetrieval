5e5990a24efc3eafaa38ac4e5607b7007cdd323c
unknown <ldecicco@usgs.gov>
2015-02-18 11:12:25 -0600

    Added unit tests.

--- tests/testthat/tests_general.R
+++ tests/testthat/tests_general.R
@@ -5,13 +5,16 @@ test_that("General NWIS retrievals working", {
   
   multiSite <- readNWISdata(sites=c("04025500","040263491"), service="iv", 
                             parameterCd="00060")
+  expect_is(multiSite$dateTime, 'POSIXct')
   
   bBoxEx <- readNWISdata(bBox=c(-83,36.5,-81,38.5), parameterCd="00010")
+  expect_that(length(unique(bBoxEx$site_no)) > 1, is_true())
   
   startDate <- as.Date("2013-10-01")
   endDate <- as.Date("2014-09-30")
   waterYear <- readNWISdata(bBox=c(-83,36.5,-81,38.5), parameterCd="00010", 
                    service="dv", startDate=startDate, endDate=endDate)
+  expect_is(waterYear$Date, 'Date')
   
   siteInfo <- readNWISdata(stateCd="WI", parameterCd="00010",hasDataTypeCd="iv", 
                            service="site")
