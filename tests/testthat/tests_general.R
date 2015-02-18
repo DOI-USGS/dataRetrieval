75569a8065b25ff674d02ae04380e0b8090d4128
unknown <ldecicco@usgs.gov>
2015-02-18 13:35:23 -0600

    Added tests.

--- tests/testthat/tests_general.R
+++ tests/testthat/tests_general.R
@@ -20,3 +20,22 @@ test_that("General NWIS retrievals working", {
                            service="site")
   
 })
+
+
+test_that("General WQP retrievals working", {
+  testthat::skip_on_cran()
+  
+  nameToUse <- "pH"
+  pHData <- readWQPdata(siteid="USGS-04024315",characteristicName=nameToUse)
+  expect_is(pHData$ActivityStartDateTime, 'POSIXct')
+  
+  pHDataExpanded2 <- readWQPdata(bBox=c(-90.10,42.67,-88.64,43.35),
+                                 characteristicName=nameToUse)
+  expect_is(pHDataExpanded2$ActivityStartDateTime, 'POSIXct')
+  
+  startDate <- as.Date("2013-01-01")
+  nutrientDaneCounty <- readWQPdata(countycode="US:55:025",startDate=startDate,
+                         characteristicType="Nutrient")
+  expect_is(nutrientDaneCounty$ActivityStartDateTime, 'POSIXct')
+                         
+})
