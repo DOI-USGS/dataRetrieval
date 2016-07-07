5d0c5f91e0dadd806919a488451749a4a2fd0e84
wdwatkins <wwatkins@usgs.gov>
2016-07-07 11:36:17 -0500

    added countyCdLookup function

--- R/readNWISdata.r
+++ R/readNWISdata.r
@@ -260,3 +260,42 @@ stateCdLookup <- function(input, outputType="postal"){
   
   return(retVal)
 }
+
+#' County code look up 
+#'
+#' Function to simplify finding county and county code definitions. Used in \code{readNWISdata}
+#' and \code{readNWISuse}.
+#'
+#' @param state could be character (full name, abbreviation, id), or numeric (id)
+#' @param county could be character (name, with or without "County") or numeric (id)
+#' @param outputType character can be "fullName","tableIndex", "id", or "fullEntry". 
+#' @export
+#' @examples
+#' id <- countyCdLookup(state = "WI", county = "Dane")
+#' name <- countyCdLookup(state = "OH", county = 13, output = "fullName")
+#' index <- countyCdLookup(state = "Pennsylvania", county = "ALLEGHENY COUNTY", output = "tableIndex")
+#' fromIDs <- countyCdLookup(state = 13, county = 5, output = "fullName")
+
+countyCdLookup <- function(state, county, outputType = "id"){
+  outputType <- match.arg(outputType, c("fullName","tableIndex","id","fullEntry"))
+  
+  #first turn state into stateCd postal name
+  stateCd <- stateCdLookup(state,outputType = "postal")
+  
+  if(is.numeric(county) | !is.na(suppressWarnings(as.numeric(county)))){
+    county <- which(county == as.numeric(countyCd$COUNTY) & stateCd == countyCd$STUSAB)
+  } else {
+    #check if "County" was included on string - need it to match countyCd data frame
+    county <- ifelse(!grepl('(?i)\\County$',county),paste(county,"County"),county)
+    county <- which(tolower(county) == tolower(countyCd$COUNTY_NAME) & stateCd == countyCd$STUSAB)
+  }
+  
+  retVal <- switch(outputType,
+                   fullName = countyCd$COUNTY_NAME[county],
+                   tableIndex = county,
+                   id = countyCd$COUNTY[county],
+                   fullEntry = countyCd[county,]
+  )
+  
+  return(retVal)
+}
