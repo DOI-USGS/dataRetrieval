4b42a5a898af42f8ac70189ebf1e2a128a7a27ac
wdwatkins <wwatkins@usgs.gov>
2016-07-26 10:13:49 -0500

    Fixed bug with new readr version

--- R/importRDB1.r
+++ R/importRDB1.r
@@ -49,6 +49,11 @@
 #' @importFrom readr problems
 #' @importFrom lubridate fast_strptime
 #' @importFrom lubridate parse_date_time
+#' @importFrom lubridate seconds_to_period
+#' @importFrom lubridate second
+#' @importFrom lubridate minute
+#' @importFrom lubridate hour
+#' @import lubridate
 #' @examples
 #' siteNumber <- "02177000"
 #' startDate <- "2012-09-01"
@@ -117,6 +122,11 @@ importRDB1 <- function(obs_url, asDateTime=TRUE, convertType = TRUE, tz=""){
 
   if(convertType){
     readr.data <- suppressWarnings(read_delim(doc, skip = (meta.rows+2),delim="\t",col_names = FALSE))
+  #defaults to time in seconds in readr 0.2.2.9??  
+    if(data.class(readr.data$X4)=="hms"){
+    td <- seconds_to_period(readr.data$X4[!is.na(readr.data$X4)])
+    readr.data$X4[!is.na(readr.data$X4)] <- sprintf('%02d:%02d:%02d', hour(td), minute(td), second(td))
+    }
   } else {
     readr.data <- read_delim(doc, skip = (meta.rows+2),delim="\t",col_names = FALSE, col_types = cols(.default = "c"))
   }
@@ -167,7 +177,7 @@ importRDB1 <- function(obs_url, asDateTime=TRUE, convertType = TRUE, tz=""){
         if(all(c(paste0(i,"_dt"),paste0(i,"_tm")) %in% header.names)){
           varname <- paste0(i,"_dateTime")
 
-          varval <- as.POSIXct(fast_strptime(paste(readr.data[,paste0(i,"_dt")],readr.data[,paste0(i,"_tm")]), "%Y-%m-%d %H:%M", tz = "UTC"))
+          varval <- as.POSIXct(fast_strptime(paste(readr.data[,paste0(i,"_dt")],readr.data[,paste0(i,"_tm")]), "%Y-%m-%d %H:%M:%S", tz = "UTC"))
           
           if(!all(is.na(varval))){
             readr.data[,varname] <- varval
