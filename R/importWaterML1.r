d248db0bc7000c1b41b607e6caddbfe92916dafa
wdwatkins <wwatkins@usgs.gov>
2016-09-01 16:25:43 -0500

    all fixed

--- R/importWaterML1.r
+++ R/importWaterML1.r
@@ -178,8 +178,8 @@ importWaterML1 <- function(obs_url,asDateTime=FALSE, tz=""){
     siteCodeNode <- sourceInfo[xml_name(sourceInfo)=="siteCode"]
     site_no <- xml_text(siteCodeNode)
     siteCodeAtt <- unlist(xml_attrs(siteCodeNode))
-    siteDF <- cbind.data.frame(t(locText),t(tzInfo),siteName,t(siteCodeAtt),srs,t(siteProp),
-                               stringsAsFactors = FALSE)
+    siteDF <- cbind.data.frame(t(locText),t(tzInfo),station_nm=siteName,t(siteCodeAtt),srs,t(siteProp),
+                               site_no,stringsAsFactors = FALSE)
     defaultTZ <- xml_attr(xml_find_all(sourceInfo,".//ns1:defaultTimeZone"),"zoneAbbreviation")
     
     for(v in valParents){
@@ -308,6 +308,14 @@ importWaterML1 <- function(obs_url,asDateTime=FALSE, tz=""){
       mergedStat <- full_join(mergedStat, statDF, by = colnames(mergedStat))
     }
   }
+  #keep attribute df names the same as old version
+  names(mergedSite) <- c("dec_lat_va", "dec_lon_va", "timeZoneOffset", "timeZoneAbbreviation",
+                         "station_nm","network","agency_cd","srs","siteTypeCd",
+                         "hucCd", "stateCd", "countyCd", "site_no")
+  mergedSite <- mergedSite[c("station_nm", "site_no", "agency_cd", "timeZoneOffset", 
+                             "timeZoneAbbreviation", "dec_lat_va","dec_lon_va","srs","siteTypeCd",
+                             "hucCd","stateCd","countyCd","network")]
+  
   #move tz column to far right and sort by increasing site number to be consistent with old version
   mergedNames <- names(mergedDF)
   tzLoc <- grep("tz_cd", names(mergedDF))
