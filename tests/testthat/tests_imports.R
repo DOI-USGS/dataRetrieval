49bbb6055db3e4eaa39b0dbfd84de1478006cfa7
wdwatkins <wwatkins@usgs.gov>
2017-12-19 17:58:00 -0600

    NA data won't be there forever

--- tests/testthat/tests_imports.R
+++ tests/testthat/tests_imports.R
@@ -161,11 +161,12 @@ test_that("External importWaterML1 test", {
 
 test_that("no data val replacement works", {
   #may need to change dates and/or site if this data is estimated
-  iceSite <- "04024430"
-  naData <- readNWISuv(siteNumbers = iceSite, parameterCd = "00060",
-                       startDate = "2017-12-17", endDate = "2017-12-18")
-  expect_gt(sum(is.na(naData$X_00060_00000)), 0)
-  expect_false(any(na.omit(naData$X_00060_00000 < -99)))
+  #run this test when changing anything with ML1 parser!
+  # iceSite <- "04024430"
+  # naData <- readNWISuv(siteNumbers = iceSite, parameterCd = "00060",
+  #                      startDate = "2017-12-17", endDate = "2017-12-18")
+  # expect_gt(sum(is.na(naData$X_00060_00000)), 0)
+  # expect_false(any(na.omit(naData$X_00060_00000 < -99)))
 })
 
 context("importWaterML2")
