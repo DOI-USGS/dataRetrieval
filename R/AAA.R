5df1e1fff477d0f093b708848f8cfe235222b386
DeCicco <ldecicco@usgs.gov>
2019-04-04 15:57:29 -0500

    Giving the user the chance to set their access before loading dataRetrieval

--- R/AAA.R
+++ R/AAA.R
@@ -1,8 +1,12 @@
 pkg.env <- new.env()
 
 .onLoad = function(libname, pkgname){
-  suppressMessages(setAccess('public'))
-  options(Access.dataRetrieval = NULL)
+  if("Access.dataRetrieval" %in% names(options())){
+    suppressMessages(setAccess(options()[["Access.dataRetrieval"]]))
+  } else {
+    suppressMessages(setAccess('public'))
+    options(Access.dataRetrieval = NULL)    
+  }
 }
 
 
