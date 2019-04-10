e600e9be6f3d69b931bd3a577464e37ae8230333
DeCicco <ldecicco@usgs.gov>
2019-04-10 13:16:13 -0500

    Rebuilding site

--- hexSticker.R
+++ hexSticker.R
@@ -1,15 +1,15 @@
 library(hexSticker)
 
-outline = "#000080"
-background = "white" 
-sticker("./man/figures/USGS_R.png",
+outline = "#003366"
+background = "#003366" 
+sticker("./man/figures/highrezlogo-01.png",
         package = "dataRetrieval",
         h_fill = background,
         h_color = outline,
         s_y = 1,
         p_size = 12,
         p_y = 1.6,
-        s_width = 0.7,
-        s_height = 0.65,
+        s_width = 0.72,
+        s_height = 0.55,
         s_x = 1,
         filename = "./man/figures/logo.png")
