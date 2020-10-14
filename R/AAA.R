e7b5d61352871d8b9827aa637f9201111fb8531c
David Blodgett <dblodgett@usgs.gov>
2020-10-13 20:04:20 -0500

    quiet requireNamespace check and move web service call into findNLDI

--- R/AAA.R
+++ R/AAA.R
@@ -3,6 +3,5 @@ pkg.env <- new.env()
 .onLoad = function(libname, pkgname){
     suppressMessages(setAccess('public'))
     pkg.env$nldi_base <- "https://labs.waterdata.usgs.gov/api/nldi/linked-data/"
-    pkg.env$local_sf <- requireNamespace("sf")
-    pkg.env$current_nldi <- get_nldi_sources()
+    pkg.env$local_sf <- requireNamespace("sf", quietly = TRUE)
 }
