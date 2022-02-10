aef0beaca0a99247873bc1fa6ad4b65cc64bd78d
DeCicco <ldecicco@usgs.gov>
2022-02-10 14:33:49 -0600

    Fixes #599

--- R/readNWISqw.r
+++ R/readNWISqw.r
@@ -83,8 +83,7 @@ readNWISqw <- function (siteNumbers,parameterCd,startDate="",endDate="",
                         expanded=TRUE,reshape=FALSE,tz="UTC"){  
   
   .Deprecated(new = "readWQPqw", package = "dataRetrieval", 
-              msg = "NWIS qw web services are being retired. Please see the vignette 
-'Changes to NWIS QW services' for more information.")
+              msg = "NWIS qw web services are being retired. Please see vignette('qwdata_changes', package = 'dataRetrieval') for more information.")
   
   pgrp <- c("INF", "PHY", "INM", "INN", "NUT", "MBI", "BIO", "IMM", "IMN", "TOX",
             "OPE", "OPC", "OOT", "RAD", "XXX", "SED", "POP",
