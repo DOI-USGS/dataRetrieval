aea05dd7bed8bc9f866149e199d4249b1e57b501
Laura A DeCicco <ldecicco@usgs.gov>
2024-10-22 09:34:46 -0500

    Fix more tests

--- tests/testthat/tests_userFriendly_fxns.R
+++ tests/testthat/tests_userFriendly_fxns.R
@@ -141,13 +141,15 @@ test_that("WQP qw tests", {
   nameToUse <- "Specific conductance"
   pcodeToUse <- "00095"
 
-  INFO_WQP <- readWQPqw("USGS-04024315", pcodeToUse, startDate = "", endDate = "")
+  INFO_WQP <- readWQPqw("USGS-04024315", pcodeToUse, 
+                        startDate = "", endDate = "", legacy = FALSE)
   expect_is(INFO_WQP$Activity_StartDateTime, "POSIXct")
 
-  INFO2 <- readWQPqw("WIDNR_WQX-10032762", nameToUse, startDate = "", endDate = "")
+  INFO2 <- readWQPqw("WIDNR_WQX-10032762", nameToUse, 
+                     startDate = "", endDate = "", legacy = FALSE)
   expect_is(INFO2$Activity_StartDateTime, "POSIXct")
   
-  df <- readWQPqw("USGS-04193500", parameterCd = "00665")
+  df <- readWQPqw("USGS-04193500", parameterCd = "00665", legacy = FALSE)
   expect_true(nrow(df) > 0)
   
   df2 <- readWQPqw("USGS-05427718", parameterCd = "all")
