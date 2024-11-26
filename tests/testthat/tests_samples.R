a7ebdf6418b4692e6a6abd6c99bf9412e7bfdb48
Laura A DeCicco <ldecicco@usgs.gov>
2024-11-26 16:28:58 -0600

    Adding sample-data tests

---
+++ tests/testthat/tests_samples.R
@@ -0,0 +1,172 @@
+context("General functions")
+
+
+test_that("General samples-data retrievals work using WQP tests", {
+  testthat::skip_on_cran()
+  nameToUse <- "pH"
+  pHData <- read_USGS_samples(monitoringLocationIdentifier = "USGS-04024315",
+                        characteristic = nameToUse)
+  expect_is(pHData$Activity_StartDateTime, "POSIXct")
+
+  # testing lists:
+  startDate <- as.Date("2022-01-01")
+
+
+  secchi_ops <- check_param("observedproperty",
+                            text = "secchi")
+  
+  lakeData <- read_USGS_samples(activityStartDateLower = startDate,
+                                activityStartDateUpper  = "2024-01-01",
+                                USstate = "WI",
+                                characteristicUserSupplied = secchi_ops$observedProperty, 
+                                dataProfile = "Narrow")
+  
+  expect_true(nrow(lakeData) > 0)
+  
+  lakeSites <- read_USGS_samples(monitoringLocationIdentifier = unique(lakeData$Location_Identifier),
+                                 dataType = "Monitoring locations", 
+                                 dataProfile = "Site")
+  expect_type(lakeSites, "list")
+
+
+  rawPcode <- read_USGS_samples(monitoringLocationIdentifier = "USGS-01594440",
+                                usgsPCode = "01075")
+  
+  expect_true(all(c("url", "queryTime", "headerInfo") %in%
+                    names(attributes(rawPcode))))
+
+  
+  pHData <- read_USGS_samples(monitoringLocationIdentifier = "USGS-04024315",
+                              characteristic = "pH",
+                              dataProfile = "Narrow")
+  
+  expect_true(all(c("url", "queryTime", "headerInfo") %in%
+                    names(attributes(pHData))))
+
+  
+})
+
+
+context("samples-data samples")
+test_that("samples-data samples working", {
+  testthat::skip_on_cran()
+
+  activityInfo <- read_USGS_samples(monitoringLocationIdentifier = "USGS-01594440",
+                                dataType = "Activities")
+  expect_true(nrow(activityInfo) > 0)
+})
+
+context("samples-data project")
+test_that("samples-data project working", {
+  testthat::skip_on_cran()
+  type <- "Stream"
+  projectInfo <- read_USGS_samples(countyFips = "US:55:025",
+                                siteTypeName = type,
+                                dataType = "Projects")
+  expect_true(ncol(projectInfo) >= 0)
+})
+
+context("summary_USGS_samples")
+test_that("summary_USGS_samples working", {
+  testthat::skip_on_cran()
+  
+  site1 <- summary_USGS_samples(monitoringLocationIdentifier = "USGS-01594440")
+  expect_is(site1, "data.frame")
+
+})
+
+
+
+test_that("profiles", {
+  
+  testthat::skip_on_cran()
+  # Data profiles: "Organization Data"
+  org_data <- read_USGS_samples(
+    countyFips = "US:55:025",
+    dataType = "Organizations"
+  )
+  
+  # Data profiles: "Site Data Only"
+  site_data <- read_USGS_samples(
+    countyFips = "US:55:025",
+    dataType = "Monitoring locations"
+  )
+  
+  expect_true(all(c("ProviderName", "Location_Identifier") %in% names(site_data)))
+  
+  # Data profiles: "Project Data"
+  project_data <- read_USGS_samples(
+    countyFips = "US:55:025",
+    dataType = "Projects"
+  )
+ 
+  expect_true(all(c(
+    "OrganizationIdentifier",
+    "OrganizationFormalName"
+  ) %in% names(project_data)))
+  
+  # Data profiles: "Project Monitoring Location Weighting Data"
+  proj_mlwd <- read_USGS_samples(
+    countyFips = "US:55:025",
+    dataType = "Projects",
+    dataProfile = "Project Monitoring Location Weight"
+  )
+  
+  expect_true(all(c(
+    "OrganizationIdentifier",
+    "OrganizationFormalName"
+  ) %in% names(proj_mlwd)))
+  
+  
+  
+  # Data profiles: "Sample Results (biological metadata)"
+  samp_bio <- read_USGS_samples(
+    monitoringLocationIdentifier = "USGS-04024315",
+    dataProfile = "Basic biological",
+    dataType = "Results"
+  )
+  
+  expect_true(all(c(
+    "Org_Identifier",
+    "Org_FormalName"
+  ) %in% names(samp_bio)))
+  
+  # Data profiles: "Sample Results (narrow)"
+  samp_narrow <- read_USGS_samples(
+    monitoringLocationIdentifier = "USGS-04024315",
+    dataProfile = "Narrow",
+    dataType = "Results"
+  )
+  
+  expect_true(all(c(
+    "Org_Identifier",
+    "Org_FormalName"
+  ) %in% names(samp_narrow)))
+  
+  # Data profiles: "Sampling Activity"
+  samp_activity <- read_USGS_samples(
+    monitoringLocationIdentifier = "USGS-04024315",
+    dataProfile = "Sample Activities",
+    dataType = "Activities"
+  )
+  
+  expect_true(all(c(
+    "Org_Identifier",
+    "Org_FormalName"
+  ) %in% names(samp_activity)))
+  
+  # Data profile: "Result Detection Quantitation Limit Data"
+  dl_data <- read_USGS_samples(
+    monitoringLocationIdentifier = "USGS-04024315",
+    dataType = "Results",
+    dataProfile = "Result Detection Quantitation Limit"
+  )
+  
+  expect_true(all(c(
+    "Org_Identifier",
+    "Org_FormalName"
+  ) %in% names(dl_data)))
+})
+
+
+
