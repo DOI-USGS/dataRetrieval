10b7cfe23a76557f2cfc36df16b84e5f8301ea8a
Laura DeCicco <ldecicco@usgs.gov>
2025-06-02 13:50:13 -0500

    more docs

--- R/walk_pages.R
+++ R/walk_pages.R
@@ -1,5 +1,9 @@
 #' Return a data frame if there's an empty response
 #' 
+#' @param return_list data frame returned from walk_pages
+#' @param properties A vector of requested columns
+#' @param service character, can be any existing collection such
+#' as "daily", "monitoring-locations", "time-series-metadata"
 #' 
 #' @return data.frame
 #' @noRd
@@ -26,6 +30,28 @@ deal_with_empty <- function(return_list, properties, service){
   return(return_list)
 }
 
+#' Rejigger and rename  
+#' 
+#' Reorder columns based on users property input.
+#' Add "service" prefix to returned "id" column.
+#' This allows better integration with other endpoints.
+#' 
+#' @param df data frame returned from walk_pages
+#' @param properties A vector of requested columns
+#' @param service character, can be any existing collection such
+#' as "daily", "monitoring-locations", "time-series-metadata"
+#' 
+#' @return data.frame
+#' @noRd
+#' @examples
+#' 
+#' df <- dataRetrieval:::deal_with_empty(data.frame(NULL), 
+#'                                       properties = c("time", "value", "id"),
+#'                                       service = "daily")  
+#' df2 <- dataRetrieval:::rejigger_cols(df, 
+#'                                      properties = c("value", "id", "time"),
+#'                                      service = "daily")
+#' 
 rejigger_cols <- function(df, properties, service){
   new_id <- paste0(gsub("-", "_", service), "_id")
   names(df)[names(df) == "id"] <- new_id
@@ -37,7 +63,31 @@ rejigger_cols <- function(df, properties, service){
   df
 }
 
-cleanup_cols <- function(df){
+
+#' Convert columns if needed
+#' 
+#' These are columns that have caused problems in testing.
+#' Mostly if the columns are empty on 1 page, but not the next.
+#' The qualifier column also comes back as a list column. This
+#' is fine for many, but others prefer a character column.
+#' 
+#' 
+#' @param df data frame returned from walk_pages
+#' @param service character, can be any existing collection such
+#' as "daily"
+#' @return data.frame
+#' @noRd
+#' @examples
+#' 
+#' df <- dataRetrieval:::deal_with_empty(data.frame(NULL), 
+#'                                       properties = c("time", "value", "id", "qualifier"),
+#'                                       service = "daily")
+#' df2 <- dataRetrieval:::rejigger_cols(df, 
+#'                                      properties = c("value", "id", "time", "qualifier"),
+#'                                      service = "daily")
+#' df3 <- dataRetrieval:::cleanup_cols(df2)
+#' 
+cleanup_cols <- function(df, service = "daily"){
   
   if("qualifier" %in% names(df)){
     if(!all(is.na(df$qualifier))){
@@ -48,7 +98,10 @@ cleanup_cols <- function(df){
   }
   
   if("time" %in% names(df)){
-    df$time <- as.Date(df$time)
+    if(service == "daily"){
+      df$time <- as.Date(df$time)
+    }
+    # leave some room here for POSIXct in the other services.
   }
   
   if("value" %in% names(df)){
@@ -62,6 +115,17 @@ cleanup_cols <- function(df){
   df
 }
 
+#' Next request URL
+#' 
+#' Custom function to find the "next" URL from the API response.
+#' @seealso [httr2::req_perform_iterative]
+#' 
+#' @param resp httr2 response from last request
+#' @param req httr2 request from last time
+#' 
+#' @noRd
+#' @return the url for the next request
+#' 
 next_req_url <- function(resp, req) {
 
   body <- httr2::resp_body_json(resp)
@@ -81,11 +145,15 @@ next_req_url <- function(resp, req) {
     
     next_url <- links[[next_index]][["href"]]
     
+    ################################################
+    # This offset check will be going away 
+    # offset should be replaced by "cursor" eventually.
     offset <- as.integer(sub("(?i).*?\\boffset=?\\s*(\\d+).*", "\\1", next_url))
     if(isTRUE(offset > 40000)){
       warning("Not all data was returned! Split up the query for best results.")
       return(NULL)
     }
+    ################################################
     
     return(httr2::req_url(req = req, url = next_url))
   } else {
@@ -93,6 +161,16 @@ next_req_url <- function(resp, req) {
   }
 }
 
+#' Get single response data frame
+#' 
+#' Depending on skipGeometry to decide to use sf or not.
+#' 
+#' @noRd
+#' 
+#' @param resp httr2 response from last request
+#' 
+#' @return data.frame
+#' 
 get_resp_data <- function(resp) {
   
   body <- httr2::resp_body_json(resp)
@@ -113,12 +191,21 @@ get_resp_data <- function(resp) {
   
 }
 
+#' Walk through the pages
+#' 
+#' @param req httr2 initial request
+#' 
+#' @noRd
+#' @return data.frame with attributes
 walk_pages <- function(req){
   
   resps <- httr2::req_perform_iterative(req, 
                                         next_req = next_req_url, 
                                         max_reqs = Inf) 
 
+  ######################################
+  # So far I haven't tested this because I haven't had 
+  # individual failures
   failures <- resps |>
     httr2::resps_failures() |>
     httr2::resps_requests()
@@ -126,6 +213,7 @@ walk_pages <- function(req){
   if(length(failures) > 0){
     message("There were", length(failures), "failed requests.")
   }
+  ######################################
   
   return_list <- data.frame()
   for(resp in resps){
