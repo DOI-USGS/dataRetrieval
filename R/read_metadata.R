8c17719399ffea5f9f4681f412bbc27772bc30f6
Laura DeCicco <ldecicco@usgs.gov>
2025-07-11 14:45:30 -0500

    new metadata stuff

---
+++ R/read_metadata.R
@@ -0,0 +1,72 @@
+#' Generalized USGS Water Meta Data API retrieval function
+#' 
+#' Function to get metadata collections from Water Data API.
+#' 
+#' @export
+#' @param collection character, can be any existing collection such
+#' as "parameter-codes", "agency-codes", "altitude-datums", "aquifer-codes",
+#' "aquifer-types", "coordinate-accuracy-codes", "coordinate-datum-codes",
+#' "coordinate-method-codes", "hydrologic-unit-codes", "medium-codes", 
+#' "national-aquifer-codes", "reliability-codes", "site-types", "statistic-codes",
+#' "topographic-codes", "time-zone-codes"
+#' @examplesIf is_dataRetrieval_user()
+#' 
+#' \donttest{
+#' agency_codes <- read_metadata("agency-codes")
+#' altitude_datums <- read_metadata("altitude-datums")
+#' aquifer_codes <- read_metadata("aquifer-codes")
+#' aquifer_types <- read_metadata("aquifer-types")
+#' coordinate_accuracy_codes <- read_metadata("coordinate-accuracy-codes")
+#' coordinate_datum_codes <- read_metadata("coordinate-datum-codes")
+#' coordinate_method_codes <- read_metadata("coordinate-method-codes")
+#' hydrologic_unit_codes <- read_metadata("hydrologic-unit-codes", max_results = 500)
+#' national_aquifer_codes <- read_metadata("national-aquifer-codes")
+#' parameter_codes <- read_metadata("parameter-codes")
+#' reliability_codes <- read_metadata("reliability-codes")
+#' site_types <- read_metadata("site-types")
+#' statistic_codes <- read_metadata("statistic-codes")
+#' topographic_codes <- read_metadata("topographic-codes")
+#' time_zone_codes <- read_metadata("time-zone-codes")
+#' }
+read_metadata <- function(collection, 
+                          max_results = NA,
+                          limit = NA){
+
+  available <- c("parameter-codes", "agency-codes", "altitude-datums", "aquifer-codes",
+                 "aquifer-types", "coordinate-accuracy-codes", "coordinate-datum-codes",
+                 "coordinate-method-codes", "hydrologic-unit-codes", "medium-codes", 
+                 "national-aquifer-codes", "reliability-codes", "site-types", "statistic-codes",
+                 "topographic-codes", "time-zone-codes")
+  
+  match.arg(collection, available)
+
+  output_id <- gsub("-", "_", collection)
+  last_letter <- substr(output_id, 
+                        start = nchar(output_id),
+                        stop = nchar(output_id))
+  if(last_letter == "s"){
+    output_id <- substr(output_id, 
+                        start = 1,
+                        stop = nchar(output_id)-1)    
+  }
+  
+  data_req <- suppressWarnings(construct_api_requests(service = collection, 
+                                                      skipGeometry = TRUE, 
+                                                      properties = NA,
+                                                      limit = limit, 
+                                                      max_results = max_results))
+  
+  return_list <- walk_pages(data_req, max_results)
+
+  return_list <- rejigger_cols(df = return_list,
+                               properties =  NA,
+                               output_id =  output_id)
+  
+  attr(return_list, "request") <- data_req
+  attr(return_list, "queryTime") <- Sys.time()
+  
+  return(return_list)
+}
+
+
+
