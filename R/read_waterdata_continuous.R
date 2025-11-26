a761b8dc81540aa069fb7a0a7b99d701a4afcd0b
Laura DeCicco <ldecicco@usgs.gov>
2025-11-26 13:38:59 -0600

    First pass

--- R/read_waterdata_continuous.R
+++ R/read_waterdata_continuous.R
@@ -13,7 +13,7 @@
 #' @param last_modified `r get_params("continuous")$last_modified`
 #' @param time_series_id `r get_params("continuous")$time_series_id`
 #' @param qualifier `r get_params("continuous")$qualifier`
-#' @param latest_continuous_id `r get_params("continuous")$id`
+#' @param continuous_id `r get_params("continuous")$id`
 #' @param properties A vector of requested columns to be returned from the query.
 #' Available options are: 
 #' `r schema <- check_OGC_requests(endpoint = "continuous", type = "schema"); paste(names(schema$properties), collapse = ", ")`
@@ -30,9 +30,6 @@
 #' allowable limit for the service.
 #' @param max_results The optional maximum number of rows to return. This value
 #' must be less than the requested limit. 
-#' @param skipGeometry This option can be used to skip response geometries for
-#' each feature. The returning object will be a data frame with no spatial
-#' information.
 #' @param convertType logical, defaults to `TRUE`. If `TRUE`, the function
 #' will convert the data to dates and qualifier to string vector.
 #' @examplesIf is_dataRetrieval_user()
@@ -51,46 +48,43 @@
 #'
 #' uv_data <- read_waterdata_continuous(monitoring_location_id = site,
 #'                            parameter_code = pcode,
+#'                            time = "P2D",
 #'                            skipGeometry = TRUE)
-#'                         
-#' uv_data_period <- read_waterdata_continuous(monitoring_location_id = site,
-#'                                   parameter_code = pcode,
-#'                                   time = "P7D")
 #' 
-#' multi_site <- read_waterdata_continuous(monitoring_location_id =  c("USGS-451605097071701",
-#'                                                           "USGS-14181500"),
-#'                               parameter_code = c("00060", "72019"),
-#'                               skipGeometry = TRUE)
 #' 
 #' # Only return data that has been modified in last 7 days         
 #' multi_site2 <- read_waterdata_continuous(monitoring_location_id =  c("USGS-451605097071701",
-#'                                                                            "USGS-14181500"),
+#'                                                                      "USGS-14181500"),
 #'                                                parameter_code = c("00060", "72019"),
 #'                                                last_modified = "P7D")
 #' 
 #' }
 read_waterdata_continuous <- function(monitoring_location_id = NA_character_,
-                                             parameter_code = NA_character_,
-                                             statistic_id = NA_character_,
-                                             properties = NA_character_,
-                                             time_series_id = NA_character_,
-                                             latest_continuous_id = NA_character_,
-                                             approval_status = NA_character_,
-                                             unit_of_measure = NA_character_,
-                                             qualifier = NA_character_,
-                                             value = NA,
-                                             last_modified = NA_character_,
-                                             skipGeometry = NA,
-                                             time = NA_character_,
-                                             bbox = NA,
-                                             limit = NA,
-                                             max_results = NA,
-                                             convertType = TRUE){
+                                      parameter_code = NA_character_,
+                                      statistic_id = NA_character_,
+                                      properties = NA_character_,
+                                      time_series_id = NA_character_,
+                                      continuous_id = NA_character_,
+                                      approval_status = NA_character_,
+                                      unit_of_measure = NA_character_,
+                                      qualifier = NA_character_,
+                                      value = NA,
+                                      last_modified = NA_character_,
+                                      # skipGeometry = NA,
+                                      time = NA_character_,
+                                      bbox = NA,
+                                      limit = NA,
+                                      max_results = NA,
+                                      convertType = TRUE){
   
   service <- "continuous"
   output_id <- "continuous_id"
   
   args <- mget(names(formals()))
+  args[["skipGeometry"]] <- TRUE
+  if(is.na(args[["limit"]])){
+    args[["limit"]] <- 40000
+  }
   return_list <- get_ogc_data(args,
                               output_id, 
                               service)
