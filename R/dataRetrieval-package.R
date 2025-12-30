0aa2f6678cf02c5fa2c6b1fa6f79e9cad44b5976
Joseph Zemmels <jzemmels@usgs.gov>
2025-12-30 16:08:46 -0700

    Add data.table version of get_statistics_data function

---
+++ R/dataRetrieval-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table :=
+#' @importFrom data.table data.table
+## usethis namespace: end
+NULL
